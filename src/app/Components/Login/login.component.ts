967cc99140daba327f2a88c85c3e31166a6c6054
Karen Baron <96082932+Dan13l4@users.noreply.github.com>
2022-09-29 22:10:05 -0500

    changin

---
+++ src/app/Components/Login/login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "login",
+    templateUrl: './login.component.html'
+})
+
+export class Login{
+    constructor(){
+        console.log("Componente");
+    }
+}
